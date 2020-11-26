1cf9f2fb9fa50d33b8dc8d542c365551ed8ed6e8
zZChopperZz <vanspykid85@gmail.com>
2020-11-26 14:11:43 +0700

    Update DrawLine.py

--- DrawLine.py
+++ DrawLine.py
@@ -117,28 +117,6 @@ class Window(Frame):
         self.canvas.create_image(0, 0, image=self.tkimage, anchor='nw')
         self.canvas.pack()
 
-
-    def preprocess_input(self, image, net_h, net_w):
-        new_h, new_w, _ = image.shape
-
-        # determine the new size of the image
-        if (float(net_w)/new_w) < (float(net_h)/new_h):
-            new_h = (new_h * net_w)/new_w
-            new_w = net_w
-        else:
-            new_w = (new_w * net_h)/new_h
-            new_h = net_h
-
-        # resize the image to the new size
-        resized = cv2.resize(image[:,:,::-1]/255., (int(new_w), int(new_h)))
-
-        # embed the image into the standard letter box
-        new_image = np.ones((net_h, net_w, 3)) * 0.5
-        new_image[int((net_h-new_h)//2):int((net_h+new_h)//2), int((net_w-new_w)//2):int((net_w+new_w)//2), :] = resized
-        new_image = np.expand_dims(new_image, 0)
-
-        return new_image
-    net_h, net_w = 416, 416
     def main_process(self):
 
         video_src = self.filename
