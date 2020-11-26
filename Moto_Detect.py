78509ed1ff84ff2200d01f7f750954968fac8165
zZChopperZz <vanspykid85@gmail.com>
2020-11-26 14:04:18 +0700

    update DrawLine.py and Moto_Detect.py
    thêm chức năng nhận diện những phương tiện vượt vạch, vẽ khung màu đỏ lên những phương tiện đó

--- Moto_Detect.py
+++ Moto_Detect.py
@@ -5,94 +5,166 @@ import time
 import cv2
 import os
 
-
-ap = argparse.ArgumentParser()
-
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="threshold when applyong non-maxima suppression")
-args = vars(ap.parse_args())
+class detect_moto():
+	def __init__(self, img):
+		self.img = img
 
 
-labelsPath = os.path.sep.join(["yolo-coco", "coco.names"])
-LABELS = open(labelsPath).read().strip().split("\n")
+	def intersection(self,p, q, r, t):
+		print(p,q,r,t)
+		(x1, y1) = p
+		(x2, y2) = q
 
+		(x3, y3) = r
+		(x4, y4) = t
 
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-	dtype="uint8")
+		a1 = y1-y2
+		b1 = x2-x1
+		c1 = x1*y2-x2*y1
 
+		a2 = y3-y4
+		b2 = x4-x3
+		c2 = x3*y4-x4*y3
 
-weightsPath = os.path.sep.join(["yolo-coco", "yolov3.weights"])
-configPath = os.path.sep.join(["yolo-coco", "yolov3.cfg"])
+		if(a1*b2-a2*b1 == 0):
+			return False
+		print((a1, b1, c1), (a2, b2, c2))
+		x = (b1*c2 - b2*c1) / (a1*b2 - a2*b1)
+		y = (a2*c1 - a1*c2) / (a1*b2 - a2*b1)
+		print((x, y))
 
+		if x1 > x2:
+			tmp = x1
+			x1 = x2
+			x2 = tmp
+		if y1 > y2:
+			tmp = y1
+			y1 = y2
+			y2 = tmp
+		if x3 > x4:
+			tmp = x3
+			x3 = x4
+			x4 = tmp
+		if y3 > y4:
+			tmp = y3
+			y3 = y4
+			y4 = tmp
 
-print("[INFO] loading DATA YOLO ...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-
-
-image = cv2.imread("images/4.png")
-(H, W) = image.shape[:2]
-
-
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
-	swapRB=True, crop=False)
-net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-end = time.time()
-
-
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
-
-
-boxes = []
-confidences = []
-classIDs = []
-
-for output in layerOutputs:
-
-	for detection in output:
-
-		scores = detection[5:]
-		classID = np.argmax(scores)
-		confidence = scores[classID]
-
-		if confidence > args["confidence"]:
-
-			box = detection[0:4] * np.array([W, H, W, H])
-			(centerX, centerY, width, height) = box.astype("int")
-
-			x = int(centerX - (width / 2))
-			y = int(centerY - (height / 2))
+		if x >= x1 and x <= x2 and y >= y1 and y <= y2 and x >= x3 and x <= x4 and y >= y3 and y <= y4:
+			return True
+		else:
+			return False
 
 
 
-			boxes.append([x, y, int(width), int(height)])
-			confidences.append(float(confidence))
-			classIDs.append(classID)
 
-idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-	args["threshold"])
+	def run(self, line):
+		ap = argparse.ArgumentParser()
+
+		ap.add_argument("-c", "--confidence", type=float, default=0.5,
+			help="minimum probability to filter weak detections")
+		ap.add_argument("-t", "--threshold", type=float, default=0.3,
+			help="threshold when applyong non-maxima suppression")
+		args = vars(ap.parse_args())
 
 
-if len(idxs) > 0:
+		labelsPath = os.path.sep.join(["yolo-coco", "coco1.names"])
+		LABELS = open(labelsPath).read().strip().split("\n")
 
-	for i in idxs.flatten():
-		# extract the bounding box 
-		(x, y) = (boxes[i][0], boxes[i][1])
-		(w, h) = (boxes[i][2], boxes[i][3])
 
-		# draw a bounding box
-		color = [int(c) for c in COLORS[classIDs[i]]]
-		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-			0.5, color, 1)
+		np.random.seed(42)
+		COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+			dtype="uint8")
 
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+
+		weightsPath = os.path.sep.join(["yolo-coco", "yolov3.weights"])
+		configPath = os.path.sep.join(["yolo-coco", "yolov3.cfg"])
+
+
+		print("[INFO] loading DATA YOLO ...")
+		net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+		
+		try:
+			image = self.img
+		except:
+			print("Can not read image")
+		# image = cv2.imread(img)
+
+
+		(H, W) = image.shape[:2]
+
+		ln = net.getLayerNames()
+		ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+		blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+			swapRB=True, crop=False)
+		net.setInput(blob)
+		start = time.time()
+		layerOutputs = net.forward(ln)
+		end = time.time()
+
+
+		print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+
+		boxes = []
+		confidences = []
+		classIDs = []
+
+		for output in layerOutputs:
+
+			for detection in output:
+
+				scores = detection[5:]
+				classID = np.argmax(scores)
+				confidence = scores[classID]
+
+				if confidence > args["confidence"]:
+
+					box = detection[0:4] * np.array([W, H, W, H])
+					(centerX, centerY, width, height) = box.astype("int")
+
+					x = int(centerX - (width / 2))
+					y = int(centerY - (height / 2))
+
+
+					boxes.append([x, y, int(width), int(height)])
+					confidences.append(float(confidence))
+					classIDs.append(classID)
+
+		idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+			args["threshold"])
+
+
+		if len(idxs) > 0:
+
+			for i in idxs.flatten():
+				if LABELS[classIDs[i]] == "motorbike":
+					# extract the bounding box 
+					(x, y) = (boxes[i][0], boxes[i][1])
+					(w, h) = (boxes[i][2], boxes[i][3])
+					# draw a bounding box
+
+					# highlight box with intersection = true
+					(rxmin, rymin) = (x, y)
+					(rxmax, rymax) = (x+w, y+h)
+					tf = False
+					tf |= self.intersection(line[0], line[1], (rxmin, rymin), (rxmin, rymax))
+					tf |= self.intersection(line[0], line[1], (rxmax, rymin), (rxmax, rymax))
+					tf |= self.intersection(line[0], line[1], (rxmin, rymin), (rxmax, rymin))
+					tf |= self.intersection(line[0], line[1], (rxmin, rymax), (rxmax, rymax))
+
+					if tf:
+						color = (255,0,0)
+					else: 
+						color = [int(c) for c in COLORS[classIDs[i]]]
+					cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+
+					text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+					cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+						0.5, color, 2)
+		return image				
+		#cv2.imshow("Image", image)
+		#cv2.waitKey(0)
+
+	
